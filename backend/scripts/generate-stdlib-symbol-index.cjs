b3888cb6ad0af910be2a1e3dee661dba96a4ecbf
honor99 <qwerhyc@163.com>
2026-05-28 17:40:26 +0800

    chore: fix 13 SmartPerfetto → CameraPerf SPDX header remnants

--- backend/scripts/generate-stdlib-symbol-index.cjs
+++ backend/scripts/generate-stdlib-symbol-index.cjs
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 // SPDX-License-Identifier: AGPL-3.0-or-later
 // Copyright (C) 2024-2026 Gracker (Chris)
-// This file is part of SmartPerfetto. See LICENSE for details.
+// This file is part of CameraPerf. See LICENSE for details.
 
 const { execFileSync } = require('child_process');
 const fs = require('fs');
